2866c3a81b03a3497617859d1b8b2d20e8b84986
ibrahim <ibra11002015@gmail.com>
2024-04-04 01:35:20 +0200

    passed the first test of the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+    let itemIndex = array.indexOf(item);
+    array.splice(itemIndex, 1);
+    return array
 };
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
