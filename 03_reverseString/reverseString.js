b689bb4d555410494101f5aa7eb57f2617e551d2
ibrahim <ibra11002015@gmail.com>
2024-04-03 03:03:55 +0200

    finished the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reversed = '';
+    for (let i = string.length-1; i >= 0; i--) {
+        reverseContainer = string.at(i);
+        reversed += reverseContainer;
+    } 
+    return reversed;
 };
+
+console.log(reverseString('hello'))
 
 // Do not edit below this line
 module.exports = reverseString;
