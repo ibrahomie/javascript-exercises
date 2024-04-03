66f198a206e5fe41898b364d3187c29fb73fabdb
ibrahim <ibra11002015@gmail.com>
2024-04-03 02:13:41 +0200

    Finished the first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    
+    if(repeats < 0) {
+        return 'ERROR';
+    }
+    let repeatedString = '';
+    for (let i = 0; i < repeats; i++){
+        repeatedString += string;
+    }
+     
+    return repeatedString;   
 };
+
+console.log(repeatString('hey', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
