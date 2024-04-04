7ca709ceeacd52b7f64aee5b3cbe4cada6e4ffb4
ibrahim <ibra11002015@gmail.com>
2024-04-04 05:28:13 +0200

    Finished the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+    let sum = 0;
+    
+    if (firstNumber < 0 || lastNumber < 0) {
+        return "ERROR";
+    } else if (typeof(firstNumber) !== 'number' || typeof(lastNumber) !== 'number') {
+        return 'ERROR'
+    }
+    
+    if (firstNumber < lastNumber ){
+        for (let i = firstNumber; i <= lastNumber; i++) {
+            sum += i;
+        }
+    }else if (firstNumber > lastNumber) {
+        for (let i = firstNumber; i >= lastNumber; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
